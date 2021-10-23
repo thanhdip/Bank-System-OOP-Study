eee08c10063d07d83e01320c8a9e524474deb1ca
Thanh Dip <thanhdipin@gmail.com>
2021-10-22 18:37:15 -0700

    Added database create tables.

--- bank_system/database.py
+++ bank_system/database.py
@@ -1,6 +1,8 @@
 from datetime import datetime
-from sqlalchemy import create_engine, Table, Column, Integer
+from sqlalchemy import create_engine, Table, Column, Integer, Float
 from sqlalchemy import String, MetaData, DateTime
+from sqlalchemy.sql import func
+from sqlalchemy.sql.schema import ForeignKey
 
 
 class BankDatabase:
@@ -10,22 +12,47 @@ class BankDatabase:
         self._meta = MetaData()
 
     def save_data(self, table_name, data_dict) -> None:
+        pass
+
+    def _create_tables(self) -> None:
         data_type_map = ((int, Integer), (str, String), (datetime, DateTime))
-        tb = Table(table_name, self._meta,
-                   Column()
-                   )
+        accounts = Table('accounts', self._meta, Column(
+            "account_id", Integer, primary_key=True, nullable=False),
+            Column("account_type", String),
+            Column("balance", Float),
+            Column("customer_id", Integer),
+            Column("created_at", DateTime(timezone=True), default=func.now()),
+            Column("savings_rate", Float),
+            Column("min_balance", Integer),
+        )
 
-    def create_table(self, table_name, data_dict) -> int:
-        pass
+        customers = Table('customers', self._meta, Column(
+            "customer_id", Integer, primary_key=True, nullable=False),
+            Column("first_name", String),
+            Column("last_name", String),
+            Column("address", String),
+            Column("created_at", DateTime(timezone=True), default=func.now()),
+        )
 
-    def create_account():
-        pass
+        employees = Table('employees', self._meta, Column(
+            "employee_id", Integer, primary_key=True, nullable=False),
+            Column("first_name", String),
+            Column("last_name", String),
+            Column("address", String),
+            Column("title", String),
+            Column("salary", Integer),
+            Column("created_at", DateTime(timezone=True), default=func.now()),
+        )
 
-    def create_employee():
-        pass
-
-    def create_customer():
-        pass
-
-    def create_service():
-        pass
+        services = Table("services", self._meta, Column(
+            "service_id", Integer, primary_key=True, nullable=False),
+            Column("service_type", String),
+            Column("customer_id", Integer),
+            Column("created_at", DateTime(timezone=True), default=func.now()),
+            Column("borrowed_amount", Float),
+            Column("interest_rate", Float),
+            Column("term", Float),
+            Column("payed", Float),
+            Column("max_limit", Integer),
+            Column("annual_fee", Integer),
+        )
