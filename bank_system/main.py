f06a26a07662fae41462b926e0539c64b55c59a1
Thanh Dip <thanhdipin@gmail.com>
2021-10-23 03:53:23 -0700

    Added start of cli

--- bank_system/main.py
+++ bank_system/main.py
@@ -6,41 +6,54 @@ from person import Employee, Customer
 from service import Loan, CreditCard
 from account import Checking, Savings
 
-# Initialize database
-# Initialize bank system
-# Give bank system database
-# Bank system can create employees, customers, accounts, and services
-# Employees
-main_db = BankDatabase("main")
 
-objs = []
-objs2 = []
-objs.append(Employee("Soren", "Kierke", "Germany", "Teller", 50000))
-objs.append(Customer("Gregory", "Nyssa", "Georgia"))
-objs.append(Loan(50000, .01, 1))
-objs.append(CreditCard(40000, .02, 1))
-objs.append(Checking(10000, 500, 1))
-objs.append(Savings(20000, 1))
+def main():
+    main_db = BankDatabase("main")
+
+    print_linebreak()
+    print("Welcome to the Bank System, Administrator.\n....\n...\n..")
+    print("What would you like to administate:")
+    choices = ["Employees", "Customers", "Accounts", "Services"]
+    print_choices(choices)
 
 
-for obj in objs:
-    res = main_db.save_data(type(obj).__name__, obj.data_dict)
-    print(res)
-    obj.id = res[0]
-    obj.created_at = res[1]
+def admin_employees():
+    print_linebreak()
+    print("Administrate EMPLOYEES..")
+    print("Choose one:")
+    employee_choices = ["Create", "Update", "Remove"]
+    print_choices(employee_choices)
 
-for obj in objs:
-    if isinstance(obj, Person):
-        obj.first_name = obj.first_name + "NEW!!"
-    if isinstance(obj, Service):
-        obj.pay(1)
-    if isinstance(obj, Account):
-        obj.withdraw(1)
-    res = main_db.save_data(type(obj).__name__, obj.data_dict)
-    print(res)
+    print("Please enter the Employee details seperated by a space \
+         in the form:")
+    print("[first_name] [last_name] [address] [title] [salary as number]")
 
-print(main_db.find_customer("Gregory", None, None))
-print(main_db.find_employee("Soren", None, None))
 
-print(main_db.get_accounts(1))
-main_db.delete_data("Employee", 1)
+def admin_customers():
+    customer_choices = ["Create", "Update", "Remove", "Administrate Accounts",
+                        "Administrate Services"]
+
+
+def admin_accounts():
+    account_choices = ["Add to customer", "Remove from customer",
+                       "Check Balance", "Withdraw", "Deposit"]
+
+
+def admin_services():
+    services_choices = ["Add to customer", "Remove from customer",
+                        "Check Details", "Deposit"]
+
+
+def print_choices(choice_list):
+    for i, choice in enumerate(choice_list):
+        print(f"[{i}] {choice}")
+
+
+def print_linebreak():
+    for _ in range(100):
+        print('=', end='')
+    print('')
+
+
+if __name__ == "__main__":
+    main()
